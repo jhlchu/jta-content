e92abd295bdb904c1cc2bdda27323200bb8b9979
jhlchu <jhlchu@gmail.com>
2020-05-28 01:32:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+'use strict';
+var request = require('request');
+const { readdirSync } = require('fs');
+const path = require('path');
+//.resolve(__dirname, file)
 
+if (process.argv.length === 2) {
+  console.error('Expected at least one argument!');
+  process.exit(1);
+}
+const spaces = process.argv[2];
+const access_token = process.argv[3];
+
+const content_url = `https://cdn.contentful.com/spaces/${spaces}/environments/master/content_types?access_token=${access_token}`;
+
+
+request.get({
+    url: content_url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is already parsed as JSON:
+      console.log(data.html_url);
+    }
+});
